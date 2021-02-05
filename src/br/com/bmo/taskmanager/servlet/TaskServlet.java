a3af391203342e6af445ec349bf5a7358158fd4e
Bruno Moreno <brunomorello7@gmail.com>
2021-02-04 21:25:38 -0300

    Initialize Repo

---
+++ src/br/com/bmo/taskmanager/servlet/TaskServlet.java
@@ -0,0 +1,29 @@
+package br.com.bmo.taskmanager.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TaskServlet
+ */
+@WebServlet("/task")
+public class TaskServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Creating new Task");
+		PrintWriter out = response.getWriter();
+		String id = request.getParameter("id");
+		out.println("<html><body>Task " + id + " Created</body></html>");
+	}
+
+}
