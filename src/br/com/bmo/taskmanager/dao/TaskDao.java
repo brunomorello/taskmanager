d5f0bc70aafefdea2f8668aec26dfa565b93d3d9
Bruno Moreno <brunomorello7@gmail.com>
2021-02-06 19:58:05 -0300

    DAO for Task Model

---
+++ src/br/com/bmo/taskmanager/dao/TaskDao.java
@@ -0,0 +1,28 @@
+package br.com.bmo.taskmanager.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.bmo.taskmanager.model.TaskModel;
+
+public class TaskDao {
+
+	private static List<TaskModel> tasks = new ArrayList<TaskModel>();
+	
+	static {
+		TaskModel task1 = new TaskModel("task_test_1");
+		TaskModel task2 = new TaskModel("task_test_2");
+		
+		TaskDao taskDao = new TaskDao();
+		taskDao.createTask(task1);
+		taskDao.createTask(task2);
+	}
+	
+	public void createTask(TaskModel task) {
+		tasks.add(task);
+	}
+	
+	public List<TaskModel> getTasks() {
+		return tasks;
+	}
+}
