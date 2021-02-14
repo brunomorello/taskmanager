0564c0abd6716cbf6b7d510a291e008030f46db1
Bruno Moreno <brunomorello7@gmail.com>
2021-02-14 11:58:08 -0300

    Added Filter implementation for jetty deploy

--- src/br/com/bmo/taskmanager/servlet/ControllerFilter.java
+++ src/br/com/bmo/taskmanager/servlet/ControllerFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -17,6 +18,14 @@ import br.com.bmo.taskmanager.actions.ControllerAction;
  * Servlet Filter implementation class ControllerFilter
  */
 public class ControllerFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	@Override
+	public void destroy() {
+	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
